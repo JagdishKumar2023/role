dd95c18353b806694f7c04f976a5134bc07aac51
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2024-12-10 20:34:36 +0530

    m

--- index.js
+++ index.js
@@ -1,47 +1,45 @@
-// // const express = require("express");
-// // const cors = require("cors");
-// // const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-// // Connect to MongoDB
-// mongoose
-//   .connect("mongodb://127.0.0.1:27017/InputF")
-//   .then(() => console.log("MongoDB connected successfully"))
-//   .catch((err) => console.error("MongoDB connection error:", err));
+// Connect to MongoDB
+mongoose
+  .connect("mongodb://127.0.0.1:27017/InputF")
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
-// // Define the Schema
-// const User = mongoose.model("User", {
-//   name: String,
-//   email: String,
-//   password: String,
-// });
+// Define the Schema
+const formSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
 
-// // Handle POST request
-// app.post("/InputF", async (req, res) => {
-//   try {
-//     const { name, email, password } = req.body;
+const User = mongoose.model("formvalues", formSchema);
 
-//     // Check if user already exists
-//     const existingUser = await User.findOne({ email });
-//     if (existingUser) {
-//       return res.status(400).json({ message: "User already exists" });
-//     }
+// Handle POST request
+app.post("/InputF", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-//     // Create a new user
-//     const newUser = await User.create({ name, email, password });
-//     res
-//       .status(201)
-//       .json({ message: "User created successfully", user: newUser });
-//   } catch (error) {
-//     res.status(500).json({ message: "Internal server error", error });
-//   }
-// });
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.json({ message: "User is already exist" });
+    }
 
-// // Start server
-// const PORT = 4002;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+    const newUser = await User.create({ name, email, password });
+    res.json({ message: "User created successfully", user: newUser });
+  } catch (error) {
+    res.json("Enternal server error", error);
+  }
+});
+
+// Start server
+const PORT = 4002;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
